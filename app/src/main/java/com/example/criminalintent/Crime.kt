994f90c916c7e0356ebf5898fe2323c429c629d5
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2019-09-26 21:28:14 -0700

    add database use and remove requires police

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -4,9 +4,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
 
+//@Entity
+//data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+//                 var title: String = "",
+//                 var date: Date = Date(),
+//                 var isSolved: Boolean = false,
+//                 var requiresPolice: Boolean = false)
+
 @Entity
 data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
                  var title: String = "",
                  var date: Date = Date(),
-                 var isSolved: Boolean = false,
-                 var requiresPolice: Boolean = false)
+                 var isSolved: Boolean = false)
--- app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -18,11 +18,11 @@ class CrimeRepository private constructor(context: Context) {
 
     private val crimeDao = database.crimeDao()
 
-    fun getCrimes(): List<Crime> = crimeDao.getCrimes()
-    //fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
+    //fun getCrimes(): List<Crime> = crimeDao.getCrimes()
+    fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
 
-    fun getCrime(id: UUID): Crime? = crimeDao.getCrime(id)
-    //fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
+    //fun getCrime(id: UUID): Crime? = crimeDao.getCrime(id)
+    fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
